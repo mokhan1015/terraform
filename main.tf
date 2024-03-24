2cd9075668f5ed81192a051fc53eb619d283593c
mokhan1015 <70786597+mokhan1015@users.noreply.github.com>
2024-03-24 05:34:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "Amazon-Linux2-Server" {
+resource "aws_instance" "Amazon-Linux2-Server2" {
   count         = 1
   ami           = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.medium"
   key_name      = "awslinux"
   tags = {
-    Name = "Amazon-Linux2-VM"
+    Name = "Amazon-Linux2-VM2"
   }
 }
 
-resource "aws_instance" "Ubuntu_1" {
+resource "aws_instance" "Ubuntu_2" {
   count         = 1
   ami           = "ami-080e1f13689e07408"
   instance_type = "t2.medium"
   key_name      = "ubuntu"
   tags = {
-    Name = "Ubuntu-VM"
+    Name = "Ubuntu-VM2"
   }
 }
